67f5b479ff4894d50315cad794dbff7998a59459
Jackie <irene.yanapouth@gmail.com>
2023-05-23 01:52:50 +0200

    first

---
+++ main.js
@@ -0,0 +1,11 @@
+document.querySelector('.btn-success');
+
+const btnSuccess = document.querySelector('.btn-success');
+console.log(btnSuccess);
+
+btnSuccess.addEventListener('click', function() {
+    cookies.style.opacity ="0";
+
+})
+
+const cookies = document.querySelector('.cookies');
